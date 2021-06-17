a19dc04c8928039ccd4b4648d0cb2efa92a8d3b1
Toby Smith <toby.d.smith@outlook.com>
2021-06-17 09:10:44 +0100

    Created new functions to allow a sudoku solve much more quickly

--- main.js
+++ main.js
@@ -37,39 +37,60 @@ function generateSudokuGrid() {
   }
 }
 function testUniquness(sudokuLine, index, type) {
+  if (sudokuLine.length != 9) {
+    return false;
+  }
   let sortedLine = sudokuLine.map((a) => a);
   sortedLine = sortedLine.sort();
   for (let i = 0; i < 9; i++) {
     if (sortedLine[i] !== i + 1) {
-      console.log(`Failed on ${type} ${index + 1}`, sudokuLine);
-      return;
+      // console.log(`Failed on ${type} ${index + 1}`, sudokuLine);
+      return false;
     }
   }
+  return true;
   // console.log(`Succeed on ${type} ${index + 1}`);
 }
 
 function sudokuRowTest(sudoku) {
-  sudoku.forEach((row, index) => testUniquness(row, index, "Row"));
+  let result = true;
+  sudoku.forEach((row, index) => {
+    let trueOrFalse = testUniquness(row, index, "Row");
+    if (!trueOrFalse) {
+      result = false;
+    }
+  });
+  return result;
 }
 
 function sudokuCollumnTest(sudoku) {
+  let result = true;
   for (let collumnIndex = 0; collumnIndex < 9; collumnIndex++) {
     const currentCollumn = [];
     for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
       currentCollumn.push(sudoku[rowIndex][collumnIndex]);
     }
-    testUniquness(currentCollumn, collumnIndex, "Collumn");
+    let trueOrFalse = testUniquness(currentCollumn, collumnIndex, "Collumn");
+    if (!trueOrFalse) {
+      result = false;
+    }
   }
+  return result;
 }
 
 function sudokuBoxTest(sudoku) {
+  let result = true;
   let boxNumber = 0;
   for (let boxRow = 0; boxRow < 8; boxRow += 3) {
     for (let boxCollumn = 0; boxCollumn < 8; boxCollumn += 3) {
       const currentBox = createBox(sudoku, [boxRow, boxRow + 2], [boxCollumn, boxCollumn + 2]);
-      testUniquness(currentBox, boxNumber++, "Box");
+      let trueOrFalse = testUniquness(currentBox, boxNumber++, "Box");
+      if (!trueOrFalse) {
+        result = false;
+      }
     }
   }
+  return result;
 }
 
 function createBox(sudoku, rowLimit, collumnLimit) {
@@ -83,9 +104,12 @@ function createBox(sudoku, rowLimit, collumnLimit) {
 }
 
 function testSudoku(sudoku) {
-  sudokuRowTest(sudoku);
-  sudokuCollumnTest(sudoku);
-  sudokuBoxTest(sudoku);
+  let rowTest = sudokuRowTest(sudoku);
+  let collumnTest = sudokuCollumnTest(sudoku);
+  let boxTest = sudokuBoxTest(sudoku);
+  let isSudokuCorrect = rowTest && collumnTest && boxTest;
+  // console.log(isSudokuCorrect);
+  return isSudokuCorrect;
 }
 
 testSudoku(sudoku1);
@@ -138,3 +162,73 @@ clearButton.innerText = "Clear";
 clearButton.addEventListener("click", function () {
   exampleSudoku(emptySudoku);
 });
+
+function solveSudoku(sudoku) {
+  let nextCell = nextEmptyCell(sudoku);
+
+  //No more empty cells
+  if (!nextCell) {
+    testSudoku(sudoku);
+    return sudoku;
+  }
+  let rowIndex = nextCell[0];
+  let collumnIndex = nextCell[1];
+
+  for (let cellValue = 1; cellValue <= 9; cellValue++) {
+    sudoku[rowIndex][collumnIndex] = cellValue;
+    solveSudoku(sudoku);
+  }
+}
+
+function nextEmptyCell(sudoku) {
+  for (rowIndex = 0; rowIndex < 9; rowIndex++) {
+    for (collumnIndex = 0; collumnIndex < 9; collumnIndex++) {
+      if (sudoku[rowIndex][collumnIndex] === 0) {
+        return [rowIndex, collumnIndex];
+      }
+    }
+  }
+  return null;
+}
+function checkCurrentRowWorks(sudoku, rowIndex, collumnIndex, value) {
+  let currentRow = sudoku[rowIndex];
+  let valueWorks = true;
+  currentRow.forEach((cell, index) => {
+    if (collumnIndex === index) {
+      continue;
+    }
+    if (cell === value) {
+      valueWorks = false;
+    }
+  });
+  return valueWorks;
+}
+function checkCurrentCollumnWorks(sudoku, rowIndex, collumnIndex, value) {
+  let currentCollumn = []
+  for (let i = 0; i < 9; i++) {
+    currentCollumn.push(sudoku[i][collumnIndex])
+  }
+  let valueWorks = true;
+  currentCollumn.forEach((cell, index) => {
+    if (rowIndex === index) {
+      continue;
+    }
+    if (cell === value) {
+      valueWorks = false;
+    }
+  });
+  return valueWorks;
+}
+
+function checkCurrentRowWorks(sudoku, rowIndex, collumnIndex, value) {
+  const boxRowBoundary = Math.floor(rowIndex / 3) * 3;
+  const boxCollumnBoundary = Math.floor(collumnIndex / 3) * 3;
+  const currentBox = createBox([boxRowBoundary, boxRowBoundary + 2], [boxCollumnBoundary, boxRowBoundary]);
+  let valueWorks = true;
+  currentBox.forEach((cell, index) => {
+    if (cell === value) {
+      valueWorks = false;
+    }
+  });
+  return valueWorks;
+}
